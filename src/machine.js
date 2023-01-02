18a10ce20925df99acb6c94e8e6476f7be3ccf80
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-01-02 15:02:18 +0100

    FIx

---
+++ src/machine.js
@@ -0,0 +1,32 @@
+class Machine {
+    constructor(qtiteCafe, qtiteGobelet, qtiteEau) {
+        this.qtiteEau = qtiteEau;
+        this.qtiteCafe = qtiteCafe;
+        this.qtiteGobelet = qtiteGobelet;
+        this.nombreCafeServis = 0;
+        this.argentEncaisse = 0;
+    }
+    Inserer(argent) {
+        console.log(this.qtiteCafe);
+        if (
+            argent >= 0.4 &&
+            this.qtiteCafe >= 1 &&
+            this.qtiteGobelet >= 1 &&
+            this.qtiteEau >= 1
+        ) {
+            this.argentEncaisse += argent;
+            this.nombreCafeServis += 1;
+            return 0;
+        } else {
+            return argent;
+        }
+    }
+    getCafeServis() {
+        return this.nombreCafeServis;
+    }
+    getArgentEncaisse() {
+        return this.argentEncaisse;
+    }
+}
+
+module.exports = { Machine };
--- test/test.js
+++ test/test.js
@@ -1,41 +1,41 @@
-const mach = require('../src/index.js');
+const mach = require("../src/machine");
 
-test('Insère 40 centimes', ()=> {
+test("Insère 40 centimes", () => {
     //Etant donné une machine
-    const machine = new mach.Machine(1,1,1);
+    const machine = new mach.Machine(1, 1, 1);
 
     //Quand on met 40Cent
-    machine.Inserer(0.40);
+    machine.Inserer(0.4);
 
     //Alors un café coule
     expect(machine.getCafeServis()).toBe(1);
-    expect(machine.getArgentEncaisse()).toBe(0.40);
-})
+    expect(machine.getArgentEncaisse()).toBe(0.4);
+});
 
-test('Insère moins de 0.40',()=>{
-    //Etant donné une machine 
-    const machine = new mach.Machine(1,1,1);
+test("Insère moins de 0.40", () => {
+    //Etant donné une machine
+    const machine = new mach.Machine(1, 1, 1);
 
     //Quand on met 39Cent
-    const argentRendu = machine.Inserer(0.39)
+    const argentRendu = machine.Inserer(0.39);
     //l'argent rendu doit être l'argent inséré ET aucun café ne coule
-    expect(argentRendu).toBe(0.39)
+    expect(argentRendu).toBe(0.39);
     expect(machine.getCafeServis()).toBe(0);
-})
+});
 
-test('Insère plus de 40c',()=>{
+test("Insère plus de 40c", () => {
     //Etant donné une machine
-    const machine = new mach.Machine(1,1,1);
+    const machine = new mach.Machine(1, 1, 1);
     //on insère plus de 40c
     machine.Inserer(41);
     //Alors un café coule Et l'argent encaissé aussi
-    expect(machine.getCafeServis()).toBe(1)
-    expect(machine.getArgentEncaisse()).toBe(41)
-})
+    expect(machine.getCafeServis()).toBe(1);
+    expect(machine.getArgentEncaisse()).toBe(41);
+});
 
-test('Manque Ressources',()=>{
-    const machine = new mach.Machine(0,0,0);
-    const argentRendu = machine.Inserer(0.40)
+test("Manque Ressources", () => {
+    const machine = new mach.Machine(0, 0, 0);
+    const argentRendu = machine.Inserer(0.4);
     expect(machine.getCafeServis()).toBe(0);
-    expect(argentRendu).toBe(0.40)
-})
+    expect(argentRendu).toBe(0.4);
+});
